a5368938722cfef83406e12c90f2282ab352913c
Lolbix69 <rbdproductionz@gmail.com>
2025-08-04 04:35:44 -0500

    Added decoder
    added mainB.c, which decodes note names into the number counterparts

---
+++ build/CMakeFiles/nigiwav.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nigiwav.dir/src/mainB.c.obj"
+  "CMakeFiles/nigiwav.dir/src/mainB.c.obj.d"
+  "libnigiwav.dll.a"
+  "nigiwav.exe"
+  "nigiwav.exe.manifest"
+  "nigiwav.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nigiwav.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/nigiwav.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for nigiwav.
+# This may be replaced when dependencies are built.
