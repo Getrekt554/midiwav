460131806f906a4b82f8391885c97dda225aa941
Getrekt554 <120763365+Getrekt554@users.noreply.github.com>
2025-08-03 23:11:42 -0500

    akjsldh

---
+++ build/CMakeFiles/midiwav.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/midiwav.dir/src/mainA.cpp.obj"
+  "CMakeFiles/midiwav.dir/src/mainA.cpp.obj.d"
+  "libmidiwav.dll.a"
+  "midiwav.exe"
+  "midiwav.exe.manifest"
+  "midiwav.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/midiwav.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/midiwav.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for midiwav.
+# This may be replaced when dependencies are built.
