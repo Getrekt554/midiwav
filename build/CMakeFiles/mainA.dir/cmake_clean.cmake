1126cb35d2be2814633057a19fc03ae90d1b2311
Getrekt554 <120763365+Getrekt554@users.noreply.github.com>
2025-08-03 22:21:26 -0500

    the beginning

---
+++ build/CMakeFiles/mainA.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainA.dir/src/mainA.cpp.obj"
+  "CMakeFiles/mainA.dir/src/mainA.cpp.obj.d"
+  "libmainA.dll.a"
+  "mainA.exe"
+  "mainA.exe.manifest"
+  "mainA.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
